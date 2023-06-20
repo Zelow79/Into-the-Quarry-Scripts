942b76aede72184c1b6c67a862b934d9d1d55b21
Zelow79 <zelow79@gmail.com>
2023-06-19 23:58:22 -0400

    Update perfect-auto-button.js
    teehee

--- perfect-auto-button.js
+++ perfect-auto-button.js
@@ -1,5 +1,5 @@
 function launchClick(){
-  const orig = Math.sqrt; Math.sqrt = () => 0.5, orig2 = Math.random, Math.random = () => 0.22;
+  const orig = Math.sqrt, orig2 = Math.random; Math.sqrt = () => 0.5; Math.random = () => 0.22;
   for (let i = 0; i < 100; i++) document.getElementById('button').click();
   Math.sqrt = orig; Math.random = orig2;
 }
