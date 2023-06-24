88a29257ce4e474937c4dcb32a493ec4c8bee767
Zelow79 <zelow79@gmail.com>
2023-06-24 13:31:36 -0400

    Update finder.js
    Added comment to help explain some functionality

--- finder.js
+++ finder.js
@@ -1,4 +1,4 @@
-function finder(text, element = "button") {
+function finder(text, element = "button") { // returns first element found
   const context = document.getElementsByTagName(element);
   if (context.length < 1 || !context) return console.log("Finder failed. ¯\\_(ツ)_/¯");
   for (let i = 0; i < context.length; i++) {
